d2574fdf05c79e0ea1a3e60a9a8ff90f7dad297c
zwx <1257545592@qq.com>
2024-06-19 13:46:20 +0800

    feat:prepare to publish

--- ipc-client/build.gradle.kts
+++ ipc-client/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import com.vanniktech.maven.publish.SonatypeHost
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 android {
@@ -40,4 +43,47 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    configure(
+        AndroidSingleVariantLibrary(
+            // the published variant
+            variant = "release",
+            // whether to publish a sources jar
+            sourcesJar = true,
+            // whether to publish a javadoc jar
+            publishJavadocJar = true,
+        )
+    )
+
+    coordinates("io.github.zhangwenxue", "ipc-client", "1.0.0-alpha")
+
+    pom {
+        name.set("Android-IPC-Client")
+        description.set("An Android IPC client SDK")
+        inceptionYear.set("2024")
+        url.set("https://github.com/zhangwenxue/ipc/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("zwx")
+                name.set("zhangwenxue")
+                url.set("https://github.com/zhangwenxue/")
+            }
+        }
+        scm {
+            url.set("https://github.com/zhangwenxue/ipc/")
+            connection.set("scm:git:git://github.com/zhangwenxue/ipc.git")
+            developerConnection.set("scm:git:ssh://git@github.com/zhangwenxue/ipc.git")
+        }
+    }
+    signAllPublications()
 }
--- ipc-server/build.gradle.kts
+++ ipc-server/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import com.vanniktech.maven.publish.SonatypeHost
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 android {
@@ -33,12 +36,53 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation("io.github.classgraph:classgraph:4.8.173")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    configure(
+        AndroidSingleVariantLibrary(
+            // the published variant
+            variant = "release",
+            // whether to publish a sources jar
+            sourcesJar = true,
+            // whether to publish a javadoc jar
+            publishJavadocJar = true,
+        )
+    )
+
+    coordinates("io.github.zhangwenxue", "ipc-server", "1.0.0-alpha")
+
+    pom {
+        name.set("Android-IPC-Server")
+        description.set("An Android IPC Server SDK")
+        inceptionYear.set("2024")
+        url.set("https://github.com/zhangwenxue/ipc/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("zwx")
+                name.set("zhangwenxue")
+                url.set("https://github.com/zhangwenxue/")
+            }
+        }
+        scm {
+            url.set("https://github.com/zhangwenxue/ipc/")
+            connection.set("scm:git:git://github.com/zhangwenxue/ipc.git")
+            developerConnection.set("scm:git:ssh://git@github.com/zhangwenxue/ipc.git")
+        }
+    }
+    signAllPublications()
 }
