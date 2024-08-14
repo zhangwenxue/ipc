8fb52dc22b9dbf473e7bd26db24eaffed83c0149
zhangwenxue <1257545592@qq.com>
2024-08-14 10:38:56 +0800

    feat:add fileprovider sample

--- app/src/main/java/android/boot/ipc/MainActivity.kt
+++ app/src/main/java/android/boot/ipc/MainActivity.kt
@@ -1,14 +1,18 @@
 package android.boot.ipc
 
 import android.boot.ipc.client.Envoy
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +24,47 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
+        if ("com.wwk.provider.ecg" == intent.action) {
+            Log.i("__", "com.wwk.provider.ecg")
+            val file = File("${getExternalFilesDir("ecg")}/a.xml")
+            val uri = FileProvider.getUriForFile(this, "com.wwk.ecg.fileprovider", file)
+            // val path = "${getExternalFilesDir("ecg")}/a.xml"
+            file.run {
+                createNewFile()
+                writeText("Hello,Thank you")
+
+                val contentUri =
+                    FileProvider.getUriForFile(this@MainActivity, "com.wwk.ecg.fileprovider", this)
+
+
+                // Toast.makeText(this@MainActivity,readText(), Toast.LENGTH_SHORT).show()
+
+                contentResolver.openInputStream(contentUri)?.readBytes()?.let {
+                    runOnUiThread {
+                        Toast.makeText(
+                            this@MainActivity,
+                            contentUri.toString() + String(it),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+
+                Intent(Intent.ACTION_SENDTO).apply {
+                    data = contentUri
+                    flags =
+                        Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+
+                }.let {
+                    setResult(RESULT_OK, it)
+                    finish()
+                }
+            }
+        }
+
+
         Envoy(packageName).let {
             lifecycleScope.launch {
                 it.get(this@MainActivity, "test").onFailure { Log.e("__", "${it.message}") }
--- client/src/main/java/android/boot/client/MainActivity.kt
+++ client/src/main/java/android/boot/client/MainActivity.kt
@@ -1,20 +1,42 @@
 package android.boot.client
 
 import android.boot.ipc.client.Envoy
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val envoy by lazy {
         Envoy("android.boot.ipc")
     }
+
+    //注意：此方法需要提前调用，和launch方法同时调用时可能会提示未注册该监听
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == RESULT_OK) {
+                val data = result.data?.data
+                val uri = Uri.parse("$data")
+                contentResolver.openInputStream(uri)?.readBytes()?.let {
+                    runOnUiThread {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Client:" + String(it),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +47,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
         envoy.observe(this, "sn", null) {
             Log.i("_Client1", "client1 set :${it.getOrNull()}")
         }
         findViewById<View>(R.id.random).setOnClickListener {
-            lifecycleScope.launch {
-                envoy.set(this@MainActivity, "sn", "${System.currentTimeMillis()}")
-            }
+            launcher.launch(Intent("com.wwk.provider.ecg").setPackage("android.boot.ipc"))
+//            lifecycleScope.launch {
+//                envoy.set(this@MainActivity, "sn", "${System.currentTimeMillis()}")
+//            }
         }
     }
 }
